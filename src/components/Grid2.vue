7b9bc63baa6671bcab122f4945537a30636731e7
femigab <timfemi@gmail.com>
2022-08-08 12:26:55 +0100

    first commit

---
+++ src/components/Grid2.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+      <div class="gd">
+        <ProjectSingle
+          v-for="project in projects"
+          :key="project"
+          :date="project.date"
+          :status="project.status"
+          :description="project.description"
+          :video="project.video"
+          :url="project.url"
+          :framework="project.framework"
+          :title="project.title"
+        />
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      projects: [
+        {
+          date: "July 22, 2022",
+          status: "Online",
+          framework: "VUE.JS ",
+          title: "Cash App ",
+          description: "Manage you cash",
+          video: "Click here.....",
+          url: "https://cashapphelp.netlify.app",
+          slides: "Slides",
+        },
+        {
+          date: "July 22, 2022",
+          status: "Online",
+          framework: "VUE.JS ",
+          title: "Listing Resturant",
+          description: "Listing Resturant",
+          url: "https://listingapp.netlify.app/",
+          video: "Click here",
+          slides: "Slides",
+        },
+        {
+          date: "March 17, 2022",
+          status: "Online",
+          framework: "VUE.JS ",
+          title: "Digital Clock",
+          description: "Digital Clock",
+          url: "https://friendly-fermat-83201f.netlify.app/",
+          video: "Click here",
+          slides: "Slides",
+        },
+        {
+          date: "March 7 2022, ",
+          status: "Online",
+          framework: "JAVASCRIPT",
+          title: "Currency Calculator",
+          description: "World currency exchange calculator!",
+          url: "https://confident-hawking-810c06.netlify.app/",
+          video: "Click here",
+          slides: "Slides",
+        },
+        {
+          date: "December 22, 2021",
+          status: "Online",
+          framework: "JAVASCRIPT",
+          title: "Whack Game ",
+          description: "Whack a mole game",
+          url: "https://compassionate-mestorf-c92499.netlify.app/",
+          video: "Click here",
+          slides: "Slides",
+        },
+        {
+          date: "July 22, 2022",
+          status: "Online",
+          framework: "HTML & CSS",
+          title: "Adeori Resturant",
+          description: "Enjoy a nice meal at Adeori Resturant!",
+          url: "https://nostalgic-lovelace-acc728.netlify.app",
+          video: "Click here",
+          slides: "Slides",
+        },
+      ],
+    };
+  },
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/ProjectGrid.vue
+++ src/components/ProjectGrid.vue
@@ -16,7 +16,7 @@
     <div>
       <div class="grid-2">
         <ProjectSingle
-          v-for="project in projects"
+          v-for="project in grids"
           :key="project"
           :date="project.date"
           :status="project.status"
@@ -28,14 +28,17 @@
         />
       </div>
     </div>
+    <Grid2 />
   </div>
 </template>
 
 <script>
 import ProjectSingle from "./ProjectSingle.vue";
+import Grid2 from "./Grid2.vue";
 export default {
   components: {
     ProjectSingle,
+    Grid2,
   },
 
   data() {
@@ -62,15 +65,16 @@ export default {
           slides: "Slides",
         },
         {
-          date: "March 17, 2022",
+          date: " ",
           status: "Online",
-          framework: "VUE.JS ",
-          title: "Digital Clock",
-          description: "Digital Clock",
-          url: "https://friendly-fermat-83201f.netlify.app/",
+          framework: "JS, HTML & CSS",
+          title: "Image Galary ",
+          description: "Flex Panel",
+          url: "https://amazing-murdock-3e0b46.netlify.app/",
           video: "Click here",
           slides: "Slides",
         },
+
         {
           date: "March 7 2022, ",
           status: "Online",
@@ -102,6 +106,68 @@ export default {
           slides: "Slides",
         },
       ],
+      grids: [
+        {
+          date: "July 22, 2022",
+          status: "Online",
+          framework: "JS, HTML & CSS",
+          title: "",
+          description: "Search for all my friends",
+          url: "https://myfriendszone.netlify.app/",
+          video: "Click here",
+          slides: "Slides",
+        },
+        {
+          date: "",
+          status: "Online",
+          framework: "HTML & CSS",
+          title: "Adeori Resturant",
+          description: "Enjoy a nice meal at Adeori Resturant!",
+          url: "https://nostalgic-lovelace-acc728.netlify.app",
+          video: "Click here",
+          slides: "Slides",
+        },
+        {
+          date: " ",
+          status: "Online",
+          framework: "JS, HTML & CSS",
+          title: "Live Calendar ",
+          description: "Live calendar",
+          url: "https://upbeat-snyder-aa1648.netlify.app/",
+          video: "Click here",
+          slides: "Slides",
+        },
+        {
+          date: "March 17, 2022",
+          status: "Online",
+          framework: "VUE.JS ",
+          title: "Digital Clock",
+          description: "Digital Clock",
+          url: "https://friendly-fermat-83201f.netlify.app/",
+          video: "Click here",
+          slides: "Slides",
+        },
+        {
+          date: " ",
+          status: "Online",
+          framework: "JS, HTML & CSS",
+          title: "Image Galary ",
+          description: "Flex Panel",
+          url: "https://amazing-murdock-3e0b46.netlify.app/",
+          video: "Click here",
+          slides: "Slides",
+        },
+         {
+          date: "",
+          status: "Online",
+          framework: "JAVASCRIPT ",
+          title: "Calculator ",
+          description: "Calculator ",
+          url: "https://affectionate-stonebraker-f201de.netlify.app/",
+          video: "Click here",
+          slides: "Slides",
+        },
+      ],
     };
   },
 };
@@ -114,19 +180,17 @@ export default {
   grid-gap: 10px;
   padding-top: 60px;
 }
-.grid-1{
+.grid-1 {
   display: grid;
   grid-template-columns: 1fr;
   grid-gap: 10px;
-  
 }
-.grid-2{
+.grid-2 {
   display: grid;
   grid-template-columns: 1fr;
   grid-gap: 10px;
   margin-top: 40px;
 }
-
 
 @media (max-width: 420px) {
   .do-grid {
