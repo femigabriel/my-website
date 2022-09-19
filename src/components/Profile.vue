e47f68361fd27b497a94185e61d568e4f3a2501f
femigab <oluwafemi.gabriel@charisol.io>
2022-09-19 15:17:17 +0100

    upgrade resume updated

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -9,7 +9,7 @@
         <p>
           i'm a passionate front-end developer with a desire to learn and grow
           in a collaborative team environment. I'm Skilled in HTML, CSS, and
-          JavaScript, Vue,Js.
+          JavaScript, Vue.Js, React JS.
         </p>
       </div>
       <div class="skills">
